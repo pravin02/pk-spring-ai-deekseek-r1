147177d79dfb9681326321520d02e6106544dcf0
Pravin <prvnpatil11@gmail.com>
2025-04-02 23:24:48 +0530

    controller request enhanced

--- src/main/java/org/pk/ml/beginner/AIChatBotController.java
+++ src/main/java/org/pk/ml/beginner/AIChatBotController.java
@@ -1,24 +1,28 @@
 package org.pk.ml.beginner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/ai")
+@RequestMapping("/api/ai")
 public class AIChatBotController {
 
+    Logger LOGGER = LoggerFactory.getLogger(AIChatBotController.class);
     @Autowired
     private ChatClient chatClient;
 
-    @GetMapping("/ask")
-    public Flux<String> askToAI(@RequestParam String question) {
+    @PostMapping("/chat")
+    public Flux<String> askToAI(@RequestBody ChatRequest chatRequest) {
+        LOGGER.info("askToAI: body: {}", chatRequest);
         return chatClient.prompt()
-                .user(question)
+                .user(chatRequest.emailContent())
                 .stream().content();
     }
 }
